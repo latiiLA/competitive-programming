893243ef80138a66355f7dc8c08802a7c5d521bc
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-02-15 10:09:07 +0300

    Time: 25 ms (98.98%) | Memory: 16.5 MB (91.20%) - LeetSync

---
+++ Python_track/1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,18 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        mapped = ""
+        decripted = ""
+
+        mapping = {'1': "a", '2': "b", '3': "c", '4': "d", '5': "e", '6': "f", '7': "g", '8': "h", '9': "i", "10#": "j", "11#": "k", "12#": "l", "13#": "m", "14#": "n", "15#": "o", "16#": "p", "17#": "q", "18#": "r", "19#": "s", "20#": "t", "21#": "u", "22#": "v", "23#": "w", "24#": "x", "25#": "y", "26#": "z"}
+
+        # maps the string to the specified dictionary and created the decripted string
+        i = len(s) - 1
+        while i >= 0:
+            if s[i] == '#' and len(s) >= 3:
+                decripted += mapping[s[i-2: i+1]]
+                i = i - 3
+            else:
+                decripted += mapping[s[i]]
+                i = i - 1
+
+        return decripted[::-1]

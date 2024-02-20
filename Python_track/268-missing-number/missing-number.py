1e043df238769c8e92a8159efcb7e883a615761a
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-02-20 22:50:03 +0300

    Time: 98 ms (98.10%) | Memory: 17.8 MB (87.83%) - LeetSync

--- Python_track/268-missing-number/missing-number.py
+++ Python_track/268-missing-number/missing-number.py
@@ -1,6 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for num in range(len(nums) + 1):
-            print(num)
-            if not num in nums:
-                return num
+        # sum of all elements from 0 to n minus the sum the list to get an element that is left from the list
+
+        return (len(nums) * (len(nums) + 1) // 2) - sum(nums)

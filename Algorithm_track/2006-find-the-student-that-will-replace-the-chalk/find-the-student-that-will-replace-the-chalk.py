5938f2019f1f9f5ca159e490f0e94435ae3d8b49
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-04-02 20:48:44 +0300

    Time: 2054 ms (5.00%) | Memory: 30.6 MB (42.00%) - LeetSync

---
+++ Algorithm_track/2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -0,0 +1,21 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        n = len(chalk)
+        
+        total = sum(chalk)
+
+        if n == 1:
+            return 0
+
+        # subtract until the k is less than total sum 
+        while k >= total:
+            k -= total
+
+        # loop through the array and subtract if k is greater than arrays element, otherwise return
+        for i in range(n):
+            if k >= chalk[i]:
+                k -= chalk[i]
+            else:
+                break
+
+        return i

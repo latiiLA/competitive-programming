45e0e2fd6a760426b0a639feecb88d7f96d8ac75
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-04-11 09:53:01 +0300

    Time: 82 ms (70.97%) | Memory: 18.2 MB (6.99%) - LeetSync

--- Algorithm_track/1242-matrix-block-sum/matrix-block-sum.py
+++ Algorithm_track/1242-matrix-block-sum/matrix-block-sum.py
@@ -1,28 +1,36 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        # rows, cols = len(mat), len(mat[0])
-        # sumMat = [[0] * (cols + 1) for r in range(rows + 1)]
+        rows, cols = len(mat), len(mat[0])
+        sumMat = [[0] * (cols + 1) for r in range(rows + 1)]
 
-        # for r in range(rows):
-        #     prefix = 0
-        #     for c in range(cols):
-        #         prefix += mat[r][c]
-        #         above = sumMat[r][c + 1]
-        #         sumMat[r + 1][c + 1] = (prefix + above)
+        # Calculate prefix sum for the matrix
+        for r in range(rows):
+            prefix = 0
+            for c in range(cols):
+                prefix += mat[r][c]
+                above = sumMat[r][c + 1]
+                sumMat[r + 1][c + 1] = (prefix + above)
 
-        # print(sumMat)
+        answer = [[0] * cols for _ in range(rows)]
 
-        # answer = [[0] * cols for _ in range(rows)]
+        # Calculate the start end rows and columns. use precalculated result and update your answer
+        for i in range(rows):
+            for j in range(cols):
+                r1 = max(i - k, 0) + 1
+                c1 = max(j - k, 0) + 1
+                r2 = min(i + k, rows - 1) + 1
+                c2 = min(j + k, cols - 1) + 1
 
-        # for i in range(1, rows):
-        #     for j in range(1, cols):
-        #         answer[i][j] += prefix[i][j]
+                answer[i][j] = sumMat[r2][c2] - sumMat[r1 - 1][c2] - sumMat[r2][c1 - 1] + sumMat[r1 - 1][c1 - 1]
 
+        return answer
 
-        m, n = len(mat), len(mat[0])
-        result = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                result[i][j] = sum(sum(mat[x][max(0, j-k):min(n, j+k+1)])
-                                    for x in range(max(0, i-k), min(m, i+k+1)))
-        return result
+        # Another approach gained from leetcode solutions
+        
+        # m, n = len(mat), len(mat[0])
+        # result = [[0 for _ in range(n)] for _ in range(m)]
+        # for i in range(m):
+        #     for j in range(n):
+        #         result[i][j] = sum(sum(mat[x][max(0, j-k):min(n, j+k+1)])
+        #                             for x in range(max(0, i-k), min(m, i+k+1)))
+        # return result

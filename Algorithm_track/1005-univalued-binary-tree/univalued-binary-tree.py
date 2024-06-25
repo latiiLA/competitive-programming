92a1bfd1dfa32a7cd278a6184b8d3d0e637d8776
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-06-25 21:27:17 +0300

    Time: 37 ms (47.53%) | Memory: 16.5 MB (18.98%) - LeetSync

---
+++ Algorithm_track/1005-univalued-binary-tree/univalued-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:        
+        def bfs(root):
+            queue = deque()
+            queue.append(root)
+
+            while queue:
+                current = queue.popleft()
+                if current.val != root.val:
+                    return False
+
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+                    
+            return True
+
+        return bfs(root)

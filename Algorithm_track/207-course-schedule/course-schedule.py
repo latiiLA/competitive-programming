d642e4b789ee68eed1cc3a34eb2f8cd393768ebd
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-08-03 23:45:47 +0300

    Time: 83 ms (88.37%) | Memory: 18.5 MB (26.23%) - LeetSync

--- Algorithm_track/207-course-schedule/course-schedule.py
+++ Algorithm_track/207-course-schedule/course-schedule.py
@@ -1,25 +1,38 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        for course, prereq in prerequisites:
-            graph[course].append(prereq)
-        
-        # bfs implementation
-        visited = [0] * numCourses
-        def dfs(course):
-            if visited[course] == -1:
-                return False
-            if visited[course] == 1:
-                return True
-            visited[course] = -1
-            for prereq in graph[course]:
-                if not dfs(prereq):
-                    return False
-            visited[course] = 1
-            return True
+        indegree = [0 for _ in range(numCourses)]
 
-        # call dfs on all courses
-        for course in range(numCourses):
-            if not dfs(course):
-                return False
-        return True
+        # build the graph
+        for i in range(len(prerequisites)):
+            course, pre = prerequisites[i]
+            graph[pre].append(course)
+            indegree[course] += 1
+
+        # print(graph, indegree)
+
+        # acquire the nodes with zero indegree. that means they are independent/parent
+        queue = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        result = []
+        # implement bfs
+        # process a children of courses while adding them to the result
+        # when processing the children decrement the indegree of the childrens hence we processed their parent
+        while queue:
+            current = queue.popleft()
+            result.append(current)
+
+            for course in graph[current]:
+                indegree[course] -= 1
+
+                if indegree[course] == 0:
+                    queue.append(course)
+
+        # checks if there is a cycle
+        if len(result) != numCourses:
+            return []
+
+        return result

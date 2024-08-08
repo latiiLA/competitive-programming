deed4fb9e05d6ca9e4d714360e554b9d3284806d
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-08-09 00:04:27 +0300

    Time: 39 ms (52.53%) | Memory: 16.6 MB (11.73%) - LeetSync

--- Algorithm_track/1013-fibonacci-number/fibonacci-number.py
+++ Algorithm_track/1013-fibonacci-number/fibonacci-number.py
@@ -1,4 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        return 0 if n == 0 else 1 if n == 1 else self.fib(n - 1) + self.fib(n - 2)
+        memo = {} #memo
 
+        def dp(n: int) -> int:
+            if n < 2:
+                return n
+            if n not in memo:
+                memo[n] = dp(n - 1) + dp(n - 2)
+            return memo[n]  # Return the memoized value
+
+        return dp(n)

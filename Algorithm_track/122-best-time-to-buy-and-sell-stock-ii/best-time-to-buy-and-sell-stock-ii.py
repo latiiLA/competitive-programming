53433332cd7bea17e0726030e938ece88c704b5a
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-08-20 22:33:55 +0300

    Time: 98 ms (5.07%) | Memory: 26.3 MB (5.07%) - LeetSync

--- Algorithm_track/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ Algorithm_track/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Approach 1: loop
-        max_profit = 0
+        n = len(prices)
+        memoization = {}
 
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i - 1]:
-                max_profit += prices[i] - prices[i - 1]
+        def dp(i, holding):
+            if i >= n:
+                return 0  # Base case: No more days, no more profit
 
-        return max_profit
+            if (i, holding) in memoization:
+                return memoization[(i, holding)]
+
+            do_nothing = dp(i + 1, holding)
+            if holding:
+                do_something = dp(i + 1, False) + prices[i] # sell
+            else:
+                do_something = dp(i + 1, True) - prices[i] # buy
+
+            memoization[(i, holding)] = max(do_nothing, do_something)
+            return memoization[(i, holding)]
+
+        # Start with not holding any stock
+        return dp(0, False)

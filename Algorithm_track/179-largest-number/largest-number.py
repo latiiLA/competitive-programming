0bc20e8bfde1edef852f468e19e7ac83bb70c296
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-08-23 21:06:09 +0300

    Time: 54 ms (10.27%) | Memory: 16.6 MB (32.27%) - LeetSync

--- Algorithm_track/179-largest-number/largest-number.py
+++ Algorithm_track/179-largest-number/largest-number.py
@@ -1,10 +1,12 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        # change the list to a string then reverse sort, handle the case where all elements are 0
-        nums = [str(element) for element in nums]
-        nums.sort(key = lambda x: x * 10, reverse = True)
-        
-        if nums[0] == '0':
-            return '0'
+        nums = list(map(str, nums))
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if nums[i] + nums[j] < nums[j] + nums[i]:
+                    nums[i], nums[j] = nums[j], nums[i]
 
-        return ''.join(nums)
+        if nums[0] == '0':
+            return "0"
+
+        return "".join(nums)

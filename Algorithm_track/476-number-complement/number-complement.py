60a5cd88c4dd54f1bac585a7f0fe2f22fefed400
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-08-29 22:22:30 +0300

    Time: 42 ms (8.46%) | Memory: 16.5 MB (12.61%) - LeetSync

--- Algorithm_track/476-number-complement/number-complement.py
+++ Algorithm_track/476-number-complement/number-complement.py
@@ -1,10 +1,12 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        complement = deque()
+        complement = 0
+        exponent = 0
 
         while num > 0:
-            comp = "1" if num&1 == 0 else "0"
-            complement.appendleft(comp)
+            comp = 1 if num&1 == 0 else 0
+            complement += comp  * 2 ** exponent
+            exponent += 1            
             num >>= 1
 
-        return int("".join(complement), 2)
+        return complement

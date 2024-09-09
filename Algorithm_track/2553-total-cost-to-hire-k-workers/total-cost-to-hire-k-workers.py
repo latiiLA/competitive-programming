6315cd3a9e37fd04b29918e3d6b4f7e51b8af71d
latiiLA <49594109+latiiLA@users.noreply.github.com>
2024-09-09 14:07:56 +0300

    Time: 729 ms (21.83%) | Memory: 27.5 MB (41.37%) - LeetSync

---
+++ Algorithm_track/2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -0,0 +1,69 @@
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        n = len(costs)
+        counts = Counter(costs) # helps us to remove remove duplicate elements we have used for one one of the heap, to be removed on the second heap
+        # create two left and right heap as mentioned on the hint
+        leftCost = []
+        rightCost = []
+        
+        i = 0
+        while i < candidates:
+            heappush(leftCost, costs[i])
+            i += 1
+        
+        j = n - 1
+        while j > n - candidates - 1:
+            heappush(rightCost, costs[j])
+            j -= 1
+        
+        # print(leftCost, rightCost, i, j)
+        totalCost = 0
+
+        # take the minimum of the two heaps and add new element if you costs array is not empty to maintain the k heap size
+        while i <= j or k > 0:
+            while leftCost and counts[leftCost[0]] == 0:
+                heappop(leftCost)
+                if i <= j:
+                    heappush(rightCost, costs[i])
+                    i += 1
+            while rightCost and counts[rightCost[0]] == 0:
+                heappop(rightCost)
+                if i <= j:
+                    heappush(leftCost, costs[j])
+                    j -= 1
+
+            if not rightCost and leftCost:
+                if counts[leftCost[0]] > 0:
+                    counts[leftCost[0]] -= 1
+                totalCost += heappop(leftCost)
+                if i <= j:
+                    heappush(leftCost, costs[i])
+                    i += 1
+            elif not leftCost and leftCost:
+                totalCost += heappop(rightCost)
+                if i <= j:
+                    heappush(rightCost, costs[j])
+                    j -= 1
+
+            elif (leftCost and leftCost[0] <= rightCost[0]):
+                if counts[leftCost[0]] > 0:
+                    counts[leftCost[0]] -= 1
+                totalCost += heappop(leftCost)
+                if i <= j:
+                    heappush(leftCost, costs[i])
+                    i += 1
+            else:
+                if (rightCost and counts[rightCost[0]] > 0):
+                    counts[rightCost[0]] -= 1
+                
+                totalCost += heappop(rightCost)
+                if i <= j:
+                    heappush(rightCost, costs[j])
+                    j -= 1
+
+            # print(leftCost, rightCost, totalCost)
+            k -= 1
+            if k == 0:
+                break            
+
+        return totalCost
